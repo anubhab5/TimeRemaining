b26207a277146d4c0704fb8b1bd6fc1882e4f4ec
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2021-07-31 01:24:27 +0530

    First

---
+++ app.js
@@ -0,0 +1,27 @@
+// const moment = require("moment");
+
+(function () {
+  setInterval(() => calculateRemainingTime(), 1000);
+
+  function calculateRemainingTime() {
+    const totalTimeAvailableInSeconds = 86400;
+    let currentTime = moment().format("LTS");
+
+    if (currentTime.indexOf(":") === 1) {
+      currentTime = "0" + currentTime;
+    }
+
+    let meridiem = currentTime.substr(9, 12).toLowerCase();
+    let hour = Number(currentTime.substr(0, 2));
+    let minute = Number(currentTime.substr(3, 2));
+    let second = Number(currentTime.substr(6, 2));
+
+    if (meridiem === "am" && hour === 12) hour -= 12;
+    else if (meridiem === "pm") hour += 12;
+
+    let secondsElapsed = hour * 3600 + minute * 60 + second;
+    const remainingTimeInSeconds = totalTimeAvailableInSeconds - secondsElapsed;
+    const timerId = document.getElementById("timer").innerHTML = remainingTimeInSeconds;
+
+  }
+})();
